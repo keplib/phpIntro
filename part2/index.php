e59d2854357e52f37c880aa871e4f5f72cf67efd
keplib <keplib@gmail.com>
2024-09-09 12:17:15 +0200

    create an SQL connection and fetch data

--- part2/index.php
+++ part2/index.php
@@ -1,7 +1,7 @@
 <?php 
 require "functions.php";
 
-require 'router.php';
+// require 'router.php';
 
 
 // if ($uri === '/') {
@@ -13,3 +13,44 @@ require 'router.php';
 // }
 
 
+// connect to our MySQL database. PDO PHP data objects
+
+// First, create a new PDO class, dsn is the connection link
+$dsn ="mysql:host=127.0.0.1;port=3306;dbname=myapp;user=root;password=yourpassword;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+// we create a statement that is essentially an SQL query
+$statement = $pdo->prepare("select * from posts");
+// we execute it
+$statement->execute();
+// and we fetch the results as associative arrays
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// we can just console log it
+// dd($posts);
+
+
+// or we can fetch it as HTML list items 
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+
+
+// INTRO TO OBJECTS
+
+// class Person {
+//     public $name;
+//     public $age;
+
+//     public function breathe() {
+//         echo $this->name . ' is breathing';
+//     }
+// }
+
+
+// $person = new Person();
+
+// $person->name = 'John Doe';
+// $person->age= 25;
+// $person->breathe();
