0221e8c39ac77ac70fc9783c42058ecd2af0b80b
keplib <keplib@gmail.com>
2024-09-10 17:38:23 +0200

    notesapp: render notes on page

--- part2/controllers/notes.php
+++ part2/controllers/notes.php
@@ -1,4 +1,17 @@
 <?php
 
 $heading = 'Notes';
+
+$config = require('config.php');
+
+$db = new Database($config['database']);
+$query = "select * from notes";
+
+// When we receive several items, we do fetchAll and we fetch it as an associative array
+$posts = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+// dd($posts);
+
+
+
 require "views/notes.view.php";
--- part2/views/notes.view.php
+++ part2/views/notes.view.php
@@ -6,6 +6,11 @@
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <p>This is the notes view</p>
+            <?php 
+            foreach ($posts as $post) {
+                echo "<li>" . $post['body'] . "</li>";
+                    }
+            ?>
             </div>
         </main>
         <?php require('partials/footer.php') ?>
